3f19923e75895dbc1785a28e8dedef72b3b04b9e
rachael-codes <120269685+rachael-codes@users.noreply.github.com>
2024-04-01 22:49:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
-#include <limits>    // Include the <limits> header for numeric_limits
-#include <algorithm> // Include the <algorithm> header for max
+#include <limits>    
+#include <algorithm> 
 using namespace std;
 
 const int MAX_EMPLOYEES = 100; // Define MAX_EMPLOYEES constant
 
 // Structure to store employee details
-struct Employee {  // Employee is a structure (struct) that represents a collection of related data fields.
+struct Employee {  
     int id;
     string name;
     float salary;
@@ -16,14 +16,13 @@ struct Employee {  // Employee is a structure (struct) that represents a collect
 
 // Function prototypes
 void displayMainMenu();
-void addEmployee(Employee employees[], int& employeeCount); // Employee employees[]: This is an array of Employee structures. The square brackets [] indicate that employees is an array. Since no size is specified inside the brackets, it means that the size of the array is not fixed and will be determined by the caller
-void viewAllEmployees(Employee employees[], int employeeCount); // int& employeeCount: This is a reference to an integer (int&). The ampersand & indicates that employeeCount is a reference, which means that changes made to it inside the function will affect the original variable outside the function. This is typically used to allow a function to modify a variable passed to it.
+void addEmployee(Employee employees[], int& employeeCount); 
+void viewAllEmployees(Employee employees[], int employeeCount); 
 void checkTotalPay(Employee employees[], int employeeCount);
 void editEmployee(Employee employees[], int employeeCount);
 void deleteEmployee(Employee employees[], int& employeeCount);
 
 // Function that manages an employee management system, including user interaction and data manipulation
-
 int main() {
     Employee employees[MAX_EMPLOYEES];
     int employeeCount = 0;
@@ -33,7 +32,7 @@ int main() {
         displayMainMenu();
 
         int choice;
-        cin >> choice; // cin is an object of the istream class, which is used for reading input from the standard input stream (usually the keyboard)
+        cin >> choice; 
 
         switch (choice) {
             case 1:
@@ -66,12 +65,12 @@ int main() {
 // Function to display the main menu
 void displayMainMenu() {
     cout << "\nEmployee Management System" << endl;
-    cout << "1. Add new employee" << endl; // "<<": This is the insertion operator in C++. It is used to insert data into the output stream.
+    cout << "1. Add new employee" << endl; 
     cout << "2. View all employees" << endl;
     cout << "3. Check total pay" << endl;
     cout << "4. Edit employee information" << endl;
     cout << "5. Delete employee" << endl;
-    cout << "6. Exit" << endl; // "endl": This is an object in C++ that represents the end of a line. When endl is inserted into the output stream, it not only inserts a newline character (\n) but also flushes the output buffer, ensuring that the text is displayed immediately.
+    cout << "6. Exit" << endl; 
     cout << "\nEnter your choice: ";
 }
 
@@ -123,7 +122,7 @@ void addEmployee(Employee employees[], int& employeeCount) {
     }
 }
 
-
+// Function to view all employees
 void viewAllEmployees(Employee employees[], int employeeCount) {
     if (employeeCount == 0) {
         cout << "No employees to display." << endl;
@@ -137,6 +136,7 @@ void viewAllEmployees(Employee employees[], int employeeCount) {
     }
 }
 
+// Function to Check Total Pay
 void checkTotalPay(Employee employees[], int employeeCount) {
     if (employeeCount == 0) {
         cout << "No employees to calculate total pay." << endl;
@@ -161,6 +161,7 @@ void checkTotalPay(Employee employees[], int employeeCount) {
     }
 }
 
+// Function to edit an employee
 void editEmployee(Employee employees[], int employeeCount) {
     if (employeeCount == 0) {
         cout << "No employees to edit." << endl;
@@ -168,7 +169,7 @@ void editEmployee(Employee employees[], int employeeCount) {
     }
 
     int id;
-    cout << "Enter the ID of the employee you want to edit: ";
+    cout << "\nEnter the ID of the employee you want to edit: ";
     cin >> id;
 
     bool found = false;
@@ -211,6 +212,7 @@ void editEmployee(Employee employees[], int employeeCount) {
     }
 }
 
+// Function to delete an employee
 void deleteEmployee(Employee employees[], int& employeeCount) {
     if (employeeCount == 0) {
         cout << "No employees to delete." << endl;
@@ -239,14 +241,3 @@ void deleteEmployee(Employee employees[], int& employeeCount) {
         cout << "Employee with ID " << id << " not found." << endl;
     }
 }
-
-
-
-
-//std is a namespace provided by the standard library that contains many standard functions, objects, and types. It's used to organize these elements and avoid naming conflicts with user-defined names.
-
-//When you see std:: in C++ code, it means that the function or object being used is part of the standard library and is located within the std namespace.
-
-//A struct is a way to define a composite data type that groups together variables of different types under a single name. It's similar to a class in object-oriented programming but with some key differences
-
-//Void is a keyword used to indicate that a function does not return a value.
